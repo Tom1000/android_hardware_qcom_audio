4c53ddbfd05501c719eec2338e260a4dfdf41938
Balázs Triszka <balika011@gmail.com>
2018-02-26 22:11:10 +0100

    hal: Add open source HAL for Elliptic Ultrasound
    Author: Balázs Triszka <balika011@gmail.com>
    Date:   Mon Feb 26 22:11:10 2018 +0100
    
        msm8996: ultrasound: Initial open source hal for Elliptic Ultrasound
    
        * Needed for proximity sensor on Xiaomi Mi MIX
    
        Change-Id: Iaef2266bc1b853d7a9d1e2a906014c6c91019d5f
        Signed-off-by: Balázs Triszka <balika011@gmail.com>
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sat Feb 10 00:28:00 2018 +0200
    
        msm8996: ultrasound: Remove unused code
    
         * Params ultrasound_set_manual_calibration and ultrasound_set_sensitivity
           do not exist.
    
        Change-Id: I0004949db19b6ab7d49f20e422984e06a970cfe9
    
    Author: Demon Singur <demonsingur@gmail.com>
    Date:   Sat Apr 21 09:08:03 2018 +0000
    
        msm8996: hal: Update ultrasound route hacks
    
        Change-Id: If002503dfba0f005f73a4455d68bbcce9d2f617e
    
    Change-Id: I798f5a8e4f25c4d192a95befd162b86495ccc178

--- hal/Android.mk
+++ hal/Android.mk
@@ -456,6 +456,11 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_FFV)), true)
     LOCAL_SRC_FILES += audio_extn/ffv.c
 endif
 
+ifeq ($(strip $(AUDIO_FEATURE_ELLIPTIC_ULTRASOUND_SUPPORT)),true)
+    LOCAL_CFLAGS += -DELLIPTIC_ULTRASOUND_ENABLED
+    LOCAL_SRC_FILES += audio_extn/ultrasound.c
+endif
+
 LOCAL_CFLAGS += -D_GNU_SOURCE
 LOCAL_CFLAGS += -Wall -Werror
 
